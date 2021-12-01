e244f9484813c57bd44925f90a161527c63bbdad
Abiha Aftab <aaftab@xavor.com>
2021-12-01 12:16:28 +0500

    files

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
 exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions
   const { data } = await graphql(`
-		query {
+    query {
       pages: allKontentItemPageTemplate {
         nodes {
           system {
@@ -22,31 +22,44 @@ exports.createPages = async ({ actions, graphql }) => {
           }
         }
       }
-		}
-	`);
+    }
+  `)
 
-  data.pages.nodes.forEach(page => {
+  data.pages.nodes.forEach((page) => {
     const {
       system: { id: pageID },
       elements: {
         title: { value: title },
         slug: { value: pageSlug },
-        topics: { value: [{ name: category }] }
-      }
-    } = page;
-    const categorySlug = category.replace(/\s+/g, '-').toLowerCase();
-    const path = categorySlug === pageSlug ? `/${pageSlug}` : `/${categorySlug}/${pageSlug}`;
+        topics: {
+          value: [{ name: category }],
+        },
+      },
+    } = page
+    const categorySlug = category.replace(/\s+/g, '-').toLowerCase()
+    const path =
+      categorySlug === pageSlug
+        ? `/${pageSlug}`
+        : `/${categorySlug}/${pageSlug}`
     const item = {
       url: path,
       pageSlug: pageSlug,
       name: title,
       category: category,
-      categorySlug: categorySlug
+      categorySlug: categorySlug,
     }
     createPage({
       path: path,
       component: require.resolve(`./src/templates/page-template.js`),
       context: { pageID, item },
-    });
-  });
+    })
+
+    if (process.env.NODE_ENV === 'development') {
+      createPage({
+        path: `/preview/${pageSlug}`, // preview URL https://<domain>/preview/page/{Lang}/{Codename}
+        component: require.resolve(`./src/templates/page-template.js`),
+        context: { pageID, item },
+      })
+    }
+  })
 }
